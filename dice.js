71835b3fd4e7c0e49c1b8ad98d9a01ae67f2ce08
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-11-09 18:04:08 +0000

    Arreglado fallos dices.js

--- dice.js
+++ dice.js
@@ -1,4 +1,4 @@
 const MIN_DICE = 1;
-const  SIDE_DICE = 6;
+const SIDE_DICE = 6;
 const diceResult = MIN_DICE + Math.floor(Math.random() * SIDE_DICE);
-console.log(diceNumber);
+console.log(diceResult);
